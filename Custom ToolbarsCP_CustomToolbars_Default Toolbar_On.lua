f1f52525e66358f1e5ad3145beae58c138b7b052
kzarkses <cedric.pamalio@gmail.com>
2025-10-19 23:08:32 +0200

    update

--- Custom ToolbarsCP_CustomToolbars_Default Toolbar_On.lua
+++
@@ -1,6 +0,0 @@
-local r=reaper
-function EnableToolbar()
-r.SetExtState("CP_MULTI_TOOLBAR","toolbar_1751612818_9229_state","1",false)
-r.SetExtState("CP_MULTI_TOOLBAR","refresh_toolbars","1",false)
-end
-EnableToolbar()
